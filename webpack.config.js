450e76e9550f989ed26ca01e11a58473eeb53267
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2020-05-11 06:18:58 +0900

    edited js

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,45 @@
-const path = require('path');
-const webpack = require('webpack');
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: './bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    hot: true,
-    hotOnly: true,
-    watchContentBase: true,
-    watchOptions: {
-      ignored: /node_modules/
+    entry: './www/src/sample/app.jsx',
+    output: {
+        path: './www/sample/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+           {
+             test: /\.(js|jsx)$/,
+             loader: 'babel',
+             exclude: /node_modules/,
+             query: {
+               presets: ["es2015", "react"],
+             }
+           },
+           {
+             test: /\.css$/,
+             loaders: ['style', 'css?modules'],
+           }
+        ]
     }
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-};
+}
+
+//module.exports = {
+//entry: './src/index.js',
+//output: {
+//  filename: './bundle.js',
+// path: path.resolve(__dirname, 'dist')
+//},
+//devServer: {
+//  hot: true,
+//  hotOnly: true,
+//  watchContentBase: true,
+//  watchOptions: {
+//    ignored: /node_modules/
+//  }
+//},
+//plugins: [
+//  new webpack.HotModuleReplacementPlugin()
+//],
+//};
